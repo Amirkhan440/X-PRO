2aac01c7fa38da2cff6e1201fd0bf0f49eb016f9
Mr Dark <105338726+MrDarkYT@users.noreply.github.com>
2022-06-05 21:40:53 +0600

    Create OLD-CRACK.py

---
+++ OLD-CRACK.py
@@ -0,0 +1,27 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from old64 import fuck
+ 
+        fuck()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        print("Coming Soon")
